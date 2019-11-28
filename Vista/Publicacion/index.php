a5e27471c28ac73647f80902af30716206661ea0
unknown <rslopez0611@gmail.com>
2019-11-28 16:26:35 -0500

    fix data-toggle

--- Vista/Publicacion/index.php
+++ Vista/Publicacion/index.php
@@ -19,7 +19,7 @@
 			<h6 class="card-subtitle mb-2 text-muted">'.$valor["USU_FCH_RGT"].'</h6>';
 			if(isset($_SESSION["USU_ID"])) {
 				if($_SESSION["USU_ID"] == $valor["USU_ID"] || $_SESSION["USU_ROL"] == 1){
-					echo '<a href="#" class="card-link"> data-toggle="modal" data-target="editarmodal"> Editar</a>';
+					echo '<a href="#" class="card-link" data-toggle="modal" data-target="editarmodal"> Editar</a>';
 					echo '<a href="?controlador=publicacion&accion=eliminar&id='.$valor["PUB_ID"].'&catID='.$_REQUEST["id"].'" class="card-link")>Eliminar</a>';
 				}
 			}
